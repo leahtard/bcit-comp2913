a471a24af3e5570400ef8b0b1285efb861a7279b
Leah Wagner <leah@leahwagner.ca>
2019-03-09 09:23:08 -0800

    Class 3 lists exersize

---
+++ class3/class3-exersize1-lists/src/components/CarListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import CurrencyFormat from '../../node_modules/react-currency-format';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="CarListItem">
+        <tr key={this.props.price + this.props.brand + this.props.make}>
+          <td>
+            <img src={this.props.imageUrl} alt={this.props.year + " " + this.props.brand + " " + this.props.make}
+                  style={{ "width": "100px" }}/>
+          </td>
+          <td>
+            {this.props.new === true &&
+              <span style={{ "background-color": "red", "color": "white", "padding": "5px" }}>NEW</span>
+            }
+            {this.props.year} {this.props.brand} {this.props.make} <br />
+            <CurrencyFormat value={this.props.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+          </td>
+        </tr>
+      </div>
+    );
+  }
+}
+
+export default App;
